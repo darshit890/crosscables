4e595ed57bb06a31b7a91ac56f8b7ecbaccd51a3
Atharvabodkhe <atharvarb12@gmail.com>
2024-07-13 11:33:48 +0530

    modified:   components/Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,15 @@
 import Link from 'next/link'
 import React from 'react'
-
 import Image from 'next/image'
 import { NAV_LINKS } from '@/constants'
 
 const Navbar = () => {
+
   return (
     <nav className="flex justify-between px-10 py-5 items-center">
-      <Link href="/">
+      <Link href="/" className='flex items-center gap-3'>
         <Image src="/logo.jpg" alt="logo" width={75} height={75}></Image>
+        <h2 className='font-style: italic font-bold text-2xl font-sans'>Toss Automobiles</h2>
       </Link>
 
       <ul className="md:hidden h-full gap-12 lg:flex">
