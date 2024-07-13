5d5937981474a2387f4380d7da3baeb4ae7a8778
Atharvabodkhe <atharvarb12@gmail.com>
2024-07-13 18:07:58 +0530

    modified:   app/aboutUs/page.tsx 	modified:   components/ui/sticky-scroll-reveal.tsx 	new file:   public/2149367139.jpg 	new file:   public/electrical-shielded-cable-with-many-wires-generative-al.jpg 	new file:   public/image.jpg 	new file:   public/standard-quality-control-concept-m.jpg

--- app/aboutUs/page.tsx
+++ app/aboutUs/page.tsx
@@ -10,8 +10,14 @@ const content = [
     description:
       "Your premier destination for high-quality cables designed specifically for two-wheelers. With a commitment to excellence and a passion for empowering riders with reliable accessories, we strive to be your trusted partner on the road.",
     content: (
-      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--blue-100),var(--blue-200))] flex items-center justify-center text-blue-950">
-        Welcome to Toss Automobiles
+      <div className="h-full w-full  flex items-center justify-center text-white">
+        <Image
+          src="/logo.jpg"
+          width={300}
+          height={300}
+          className="h-full w-full object-cover"
+          alt="linear board demo"
+        />
       </div>
     ),
   },
@@ -20,8 +26,14 @@ const content = [
     description:
       "At Toss Automobiles, we understand the importance of safety and performance when it comes to your two-wheeled adventures. That's why we offer a comprehensive range of cables meticulously crafted to meet the demanding needs of motorcycle and scooter enthusiasts.",
     content: (
-      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--green-100),var(--green-200))] flex items-center justify-center text-blue-950">
-        Safety and Performance
+      <div className="h-full w-full  flex items-center justify-center text-white">
+        <Image
+          src="/standard-quality-control-concept-m.jpg"
+          width={300}
+          height={300}
+          className="h-full w-full object-cover"
+          alt="linear board demo"
+        />
       </div>
     ),
   },
@@ -30,8 +42,14 @@ const content = [
     description:
       "Our dedication to superior craftsmanship ensures that each cable is built to withstand the rigors of daily use, providing you with peace of mind and confidence on every ride. Whether you're a seasoned rider or just starting out, you can trust Toss Automobiles to deliver products that excel in both durability and functionality.",
     content: (
-      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--yellow-100),var(--yellow-200))] flex items-center justify-center text-blue-950">
-        Superior Craftsmanship
+      <div className="h-full w-full  flex items-center justify-center text-white">
+        <Image
+          src="/electrical-shielded-cable-with-many-wires-generative-al.jpg"
+          width={300}
+          height={300}
+          className="h-full w-full object-cover"
+          alt="linear board demo"
+        />
       </div>
     ),
   },
@@ -40,8 +58,14 @@ const content = [
     description:
       "But we're more than just a supplier of cables; we're enthusiasts ourselves. Our team is comprised of passionate riders who share your love for the open road. This firsthand experience allows us to understand the unique challenges riders face and enables us to develop cables that enhance your riding experience.",
     content: (
-      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--red-100),var(--red-200))] flex items-center justify-center text-blue-950">
-        Enthusiasts Ourselves
+      <div className="h-full w-full  flex items-center justify-center text-white">
+        <Image
+          src="/image.jpg"
+          width={300}
+          height={300}
+          className="h-full w-full object-cover"
+          alt="linear board demo"
+        />
       </div>
     ),
   },
@@ -50,8 +74,14 @@ const content = [
     description:
       "Customer satisfaction is at the core of everything we do. From our easy-to-navigate online store to our dedicated customer support team, we're here to ensure that your experience with Toss Automobiles exceeds your expectations every step of the way.",
     content: (
-      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--purple-100),var(--purple-200))] flex items-center justify-center text-blue-950">
-        Customer Satisfaction
+      <div className="h-full w-full  flex items-center justify-center text-white">
+        <Image
+          src="/2149367139.jpg"
+          width={300}
+          height={300}
+          className="h-full w-full object-cover"
+          alt="linear board demo"
+        />
       </div>
     ),
   },
