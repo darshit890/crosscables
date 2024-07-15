b0b0c3c0f25c388f1ac0f804c16ed8594da8d8d9
Atharvabodkhe <atharvarb12@gmail.com>
2024-07-15 14:19:30 +0530

    modified:   app/pay-online/_component.tsx/PaymentForm.tsx 	modified:   app/pay-online/page.tsx

--- app/pay-online/_component.tsx/PaymentForm.tsx
+++ app/pay-online/_component.tsx/PaymentForm.tsx
@@ -11,10 +11,10 @@ const PaymentForm =() => {
   return (
     <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black border mb-10">
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-        Contact Us
+        Enter the details
       </h2>
       <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-        Fill the form for contact us and will reach out to you.
+        Fill the details for payment.
       </p>
 
       <form className="my-8">
@@ -36,21 +36,13 @@ const PaymentForm =() => {
           <Label htmlFor="contactno">Contact Number</Label>
           <Input id="contactno" placeholder="1234567890" type="number" name="contactno"/>
         </LabelInputContainer>
-        <LabelInputContainer className="mb-4">
-          <Label htmlFor="subject">Subject</Label>
-          <Input id="subject" placeholder="Any Product" type="text" name="subject"/>
-        </LabelInputContainer>
-        <LabelInputContainer className="mb-4">
-          <Label htmlFor="message">Message</Label>
-          <Input id="message" placeholder="Add some thing to Ask" type="textarea" name="message"/>
-        </LabelInputContainer>
         
 
         <button
           className="bg-gradient-to-br relative group/btn from-primary dark:from-primary/80 dark:to-zinc-900 to-primary/70 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
         >
-          Submit Form &rarr;
+          Pay Now &rarr;
           <BottomGradient />
         </button>
       </form>
--- app/pay-online/page.tsx
+++ app/pay-online/page.tsx
@@ -7,19 +7,6 @@ import PaymentForm from './_component.tsx/PaymentForm'
 const PayOnlinePage = () => {
   return (
     <div className='flex flex-col justify-center mx-auto max-w-7xl'>
-        <div className="z-10 flex min-h-[5rem] items-center justify-center">
-        <div
-          className={cn(
-            "group rounded-full border border-black/5 bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800"
-          )}
-        >
-          <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
-            <span>✨ Get in Touch</span>
-            <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
-          </AnimatedShinyText>
-        </div>
-      </div>
-
       <h1 className='pointer-events-none mt-5 mb-10 whitespace-pre-wrap bg-gradient-to-b from-primary to-gray-300/80 bg-clip-text text-center text-4xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10'>Pay Online</h1>
       <PaymentForm />
     </div>
