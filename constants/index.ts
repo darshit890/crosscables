21eaa3c2d8eeb3d25ba4deea270ef828c0958849
Atharvabodkhe <atharvarb12@gmail.com>
2024-07-15 14:07:45 +0530

    modified:   constants/index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -5,4 +5,5 @@ export const NAV_LINKS = [
     { href: '/offers', key: 'offers', label: 'Offers' },
     { href: '/aboutUs', key: 'about_us', label: 'About Us' },
     { href: '/contactUs', key: 'contact_us', label: 'Contact Us' },
+    { href: '/payonline', key: 'pay_online', label: 'Pay Online' },
   ];
