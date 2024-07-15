13e2d48aa1694c23cfe3359d28bea5fac03a1a38
Atharvabodkhe <atharvarb12@gmail.com>
2024-07-15 14:37:51 +0530

    modified:   app/pay-online/_component.tsx/PaymentForm.tsx

--- app/pay-online/_component.tsx/PaymentForm.tsx
+++ app/pay-online/_component.tsx/PaymentForm.tsx
@@ -3,7 +3,7 @@ import React, { FormEventHandler } from "react";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
-import Swal from 'sweetalert2';
+
 
 
 const PaymentForm =() => {
