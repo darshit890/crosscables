3fa9e74b82b0295e8e09dd2911a41bb0f8a4f640
Atharvabodkhe <atharvarb12@gmail.com>
2024-07-20 19:12:29 +0530

    modified:   app/layout.tsx 	deleted:    public/logo.jpg 	renamed:    app/favicon.jpg -> public/logo1.jpg 	modified:   public/logo2.jpg

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.jpg" sizes="any" />
+        <link rel="icon" href="/logo.jpg" sizes="any" />
       </head>
       <body className={inter.className}>
         {children}
