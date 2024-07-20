991ae45aacce763b7fc52d6dc14c8669ad2e7733
Atharvabodkhe <atharvarb12@gmail.com>
2024-07-20 13:18:00 +0530

    modified:   components/Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,11 +9,11 @@ const NavbarAndHero: React.FC = () => {
 
   const navItems = [
     { label: 'HOME', href: '/' },
-    { label: 'OUR COMPANY', href: '/company' },
     { label: 'PRODUCTS', href: '/products' },
-    { label: 'INDUSTRY', href: '/industry' },
-    { label: 'QUALITY ASSURANCE', href: '/quality' },
-    { label: 'CONTACT US', href: '/contact' },
+    { label: 'PRICE LIST', href: '/priceList' },
+    { label: 'OFFERS', href: '/offers' },
+    { label: 'ABOUT US', href: '/aboutUs' },
+    { label: 'CONTACT US', href: '/contactUs' },
   ];
 
   return (
@@ -32,19 +32,19 @@ const NavbarAndHero: React.FC = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
-              <Image src="/logo.jpg" alt="Silco Logo" width={100} height={40} />
+              <Image src="/logo.jpg" alt="logo" width={100} height={40} />
             </div>
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
                 {navItems.map((item) => (
-                  <Link key={item.label} href={item.href} className="px-3 py-2 rounded-md text-sm font-medium">
+                  <Link key={item.label} href={item.href} className="px-3 py-2 rounded-md text-sm font-medium hover:font-semibold">
                     {item.label}
                   </Link>
                 ))}
               </div>
             </div>
             <div className="md:hidden">
-              <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+              <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="inline-flex items-center justify-center p-2 rounded-md text-gray-300 hover:text-white hover:bg-primary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                 <span className="sr-only">Open main menu</span>
                 {isMenuOpen ? (
                   <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
