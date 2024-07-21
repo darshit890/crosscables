3bf2de1a0033a42070a9c46a1e50cc989b09376f
Atharvabodkhe <atharvarb12@gmail.com>
2024-07-21 15:17:07 +0530

    modified:   components/Footer.tsx 	modified:   components/WhyChooseUs.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 import React, { useEffect, useRef, useState } from "react";
 import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
@@ -34,58 +35,58 @@ function Footer() {
   }, []);
 
   return (
-    <div 
+    <div
       ref={footerRef}
       className={cn(
         "transition-all duration-1000 ease-in-out",
-        isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
+        isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-20"
       )}
     >
-      <footer className="bg-gray-100 pt-20 pb-12 px-4 sm:px-6 lg:px-8  ">
-      <div className="max-w-7xl bg-white mx-auto border p-10 rounded-3xl">
-        <div className="flex flex-col lg:flex-row justify-between lg:px-20 items-start mb-8">
-        <div className="mb-8 md:mb-0 text-center md:text-left">
-            <ul>
-              <Image src={'/logo1.jpg'} alt="" width={130} height={60} className="mx-auto md:mx-0 mb-4" />
-              <div className="flex gap-4 md:gap-6 pb-5 justify-center md:justify-center">
-                <Link href={"https://wa.me/9898222345"}><FaWhatsapp className="text-xl md:text-2xl cursor-pointer hover:text-green-600" /></Link>
-                <Link href={"https://x.com/TossCables"}><FaXTwitter className="text-xl md:text-2xl cursor-pointer hover:text-blue-600" /></Link>
+      <footer className="bg-gray-100 pt-20 pb-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-7xl bg-white mx-auto border p-5 sm:p-10 rounded-3xl">
+          <div className="flex flex-col lg:flex-row justify-between lg:px-20 items-center lg:items-start mb-8">
+            <div className="mb-8 lg:mb-0 text-center lg:text-left">
+              <ul>
+                <Image src={'/logo1.jpg'} alt="" width={130} height={60} className="mx-auto lg:mx-0 mb-4" />
+                <div className="flex gap-4 md:gap-6 pb-5 justify-center lg:justify-start">
+                  <Link href={"https://wa.me/9898222345"}><FaWhatsapp className="text-xl md:text-2xl cursor-pointer hover:text-green-600" /></Link>
+                  <Link href={"https://x.com/TossCables"}><FaXTwitter className="text-xl md:text-2xl cursor-pointer hover:text-blue-600" /></Link>
+                </div>
+              </ul>
+            </div>
+            
+            <div className="flex flex-col sm:flex-row gap-8 sm:gap-16 mb-8 lg:mb-0">
+              <div className="p-3 md:p-5 mb-6 sm:mb-0 text-center sm:text-left">
+                <ul>
+                  <p className="font-bold text-xl md:text-2xl pb-2 md:pb-4">Quick Links</p>
+                  {NAV_LINKS.map((link) => (
+                    <Link href={link.href} key={link.key} className="flex flex-col text-gray-500 text-sm md:text-md pb-1 md:pb-2 font-semibold hover:text-primary cursor-pointer">
+                      {link.label}
+                    </Link>
+                  ))}
+                </ul>
               </div>
-            </ul>
+              <div className="p-3 md:p-5 text-center sm:text-left">
+                <ul>
+                  <p className="font-bold text-xl md:text-2xl pb-2 md:pb-4">Legal</p>
+                  {["Privacy Policy", "Terms & Conditions", "Refund Policy"].map((item) => (
+                    <li key={item} className="text-gray-500 text-sm md:text-md pb-1 md:pb-2 font-semibold hover:text-primary cursor-pointer">
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
           </div>
           
-          <div className="flex gap-16 mb-8 lg:mb-0">
-            <div className="p-3 md:p-5 mb-6 md:mb-0 text-center md:text-left">
-              <ul>
-                <p className="font-bold text-xl md:text-2xl pb-2 md:pb-4">Quick Links</p>
-                {NAV_LINKS.map((link) => (
-                  <Link href={link.href} key={link.key} className="flex flex-col text-gray-500 text-sm md:text-md pb-1 md:pb-2 font-semibold hover:text-primary cursor-pointer">
-                      {link.label}
-                  </Link>
-                ))}
-              </ul> 
+          <div className="border-t border-gray-200 pt-8">
+            <div className="flex flex-col md:flex-row justify-between items-center">
+              <p className="text-gray-600 text-center md:text-left">Copyright © 2024 Toss Cables- All Right Reserved</p>
             </div>
-            <div className="p-3 md:p-5 text-center md:text-left">
-              <ul>
-                <p className="font-bold text-xl md:text-2xl pb-2 md:pb-4">Legal</p>
-                {["Privacy Policy", "Terms & Conditions", "Refund Policy"].map((item) => (
-                  <li key={item} className="text-gray-500 text-sm md:text-md pb-1 md:pb-2 font-semibold hover:text-primary cursor-pointer">
-                    {item}
-                  </li>
-                ))}
-              </ul>
-            </div>  
-          </div>    
-        </div>
-        
-        <div className="border-t border-gray-200 pt-8">
-          <div className="flex flex-col md:flex-row justify-between items-center">
-            <p className="text-gray-600">Copyright © 2024 Toss Cables- All Right Reserved</p>
           </div>
         </div>
-      </div>
-    </footer>
-  </div>
+      </footer>
+    </div>
   );
 }
 
