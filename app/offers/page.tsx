18ca97a69ad115f6de7310c75d28b43b22826d5c
darshit890 <88980439+darshit890@users.noreply.github.com>
2024-07-26 23:28:49 +0530

    Update page.tsx

--- app/offers/page.tsx
+++ app/offers/page.tsx
@@ -5,13 +5,7 @@ import PageHero from '@/components/PageHero'
 import { cn } from '@/lib/utils'
 import { ArrowRightIcon } from 'lucide-react'
 import React from 'react'
-import { motion } from 'framer-motion'
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: "Offers",
-  description: "At Toss cables, we understand the importance of safety and performance when it comes to your two-wheeled adventures. That's why we offer a comprehensive range of cables meticulously crafted to meet the demanding needs of motorcycle and scooter enthusiasts."
-}
+import { motion } from 'framer-motion'
 
 const Offers = () => {
   return (
